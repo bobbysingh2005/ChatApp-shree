17977f09def143215985950160c30543152f188f
bobby singh <bobbysingh2005@gmail.com>
2021-05-27 00:14:59 +0530

    update1

---
+++ api/src/index.ts
@@ -0,0 +1,10 @@
+import server from './server';
+
+
+const port = Number(process.env.PORT || 3000);
+const host = process.env.HOST || 'localhost';
+server.listen(port, ()=>{
+    console.log(`
+    ChatApp shree server start on http://${host}:${port}
+    press ctrl + c to exit`)
+})
